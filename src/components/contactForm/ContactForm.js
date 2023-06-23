5aaaf6bafb4a51b7f4375cd6ae0bf8b5d3955cad
Jonathan Yau <jonathan_yau@outlook.com>
2023-06-23 17:28:58 -0400

    Add new properties to the Appointment Page included inputs and submit button

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -9,8 +9,43 @@ export const ContactForm = ({
   setEmail,
   handleSubmit
 }) => {
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    handleSubmit();
+  }
+
   return (
-    <></>
+    
+    <form onSubmit={handleFormSubmit}>
+      <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)} 
+          required
+        />
+      <label htmlFor="phone">Phone:</label>
+        <input
+          id="phone"
+          type="tel"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+          required
+        />
+      <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+      <button type="submit">Add Contact</button>
+    </form>
+
   );
 };
 
