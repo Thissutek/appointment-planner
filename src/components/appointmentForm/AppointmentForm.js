58e2506a59d95391f2b429ec4befb67f89680311
Jonathan Yau <jonathan_yau@outlook.com>
2023-06-27 15:08:14 -0400

    Updated the functionality still have a problem with the map function

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from '../contactPicker/ContactPicker'
 
 const getTodayString = () => {
   const [month, day, year] = new Date()
@@ -21,6 +22,24 @@ export const AppointmentForm = ({
 }) => {
 
   return (
-    <></>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)}/>
+      </label>
+      <label>
+        Contact:
+        <ContactPicker value={contact} onChange={(e) => setContact(e.target.value)}/>
+      </label>
+      <label>
+        Date:
+        <input type="date" value={date} onChange={(e) => setDate(e.target.value)}/>
+      </label>
+      <label>
+        Time:
+        <input type="time" value={time} onChange={(e) => setTime(e.target.value)}/>
+      </label>
+      <button type="submit">Submit</button>
+    </form>
   );
 };
